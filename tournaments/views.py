728529013a8864bf7edc9c6f9d4c75009ed50812
Marc Tamlyn <marc.tamlyn@gmail.com>
2015-03-02 15:03:43 +0000

    Add a message on Tournament submit.

--- tournaments/views.py
+++ tournaments/views.py
@@ -2,6 +2,8 @@ from django.core.urlresolvers import reverse_lazy
 from django.utils import timezone
 from django.views.generic import TemplateView, CreateView
 
+from braces.views import MessageMixin
+
 from .forms import TournamentSubmissionForm
 from .models import Tournament
 
@@ -28,7 +30,11 @@ class TournamentList(TemplateView):
         }
 
 
-class TournamentSubmit(CreateView):
+class TournamentSubmit(MessageMixin, CreateView):
     template_name = 'tournaments/tournament_submit.html'
     form_class = TournamentSubmissionForm
     success_url = reverse_lazy('tournament-list')
+
+    def form_valid(self, form):
+        self.messages.info('Your submission has been received.')
+        return super().form_valid(form)
